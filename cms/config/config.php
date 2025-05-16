36ee8402d06fb5b81b0a09db256981e4a37f561e
Ubuntu <Student@kube1.s3umcpxc5qge5cjfuwa2cb3elb.parx.internal.cloudapp.net>
2025-05-16 20:27:35 +0000

    Stable version

--- cms/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'db_host' => getenv('DB_HOST') ?: 'cms-db',
-    'db_name' => getenv('DB_NAME') ?: 'vveb',
-    'db_user' => getenv('DB_USER') ?: 'user',
-    'db_pass' => getenv('DB_PASS') ?: 'password',
-];
